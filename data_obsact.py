ffa12e43832a9da158f7db9474e5cc3fa53a18ec
mgarcas <miggarcia90@gmail.com>
2022-01-28 17:58:45 +0100

    funcion plot

--- data_obsact.py
+++ data_obsact.py
@@ -1,5 +1,4 @@
-from __future__ import print_function
-import time
+from calendar import month
 import swagger_client
 from swagger_client.rest import ApiException
 from pprint import pprint
@@ -9,6 +8,36 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+
+def plotting_data_obsact():
+    m_d = input('type month and day (mmdd): ')
+    df_data_obsact = pd.read_json('./data_obsact_{}.json'.format(m_d))
+    df_data_obsact.fint = pd.to_datetime(df_data_obsact['fint'],format='%Y-%m-%d')
+    # df['fecha'], format='%Y-%m-%d'
+
+    day = df_data_obsact.fint[0].day
+    month = df_data_obsact.fint[0].month
+
+    print('Day', day)
+    print('Month', month)
+
+    print(':_:_:_:_:_:_: DATA :_:_:_:_:_:_:')
+    print(df_data_obsact)
+
+    df_data_obsact.plot(x='fint', y=['ta', 'tpr', 'pres'], kind='line', grid=True,
+                        title='Day {}'.format(day), xlabel='Hour', secondary_y='pres')
+    
+    plt.tight_layout()
+    plt.savefig('./plot_obsact{}.png'.format(m_d))
+    # savefig(fname, dpi=None, facecolor='w', edgecolor='w',
+    #     orientation='portrait', papertype=None, format=None,
+    #     transparent=False, bbox_inches=None, pad_inches=0.1,
+    #     frameon=None)
+    plt.show()
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
 apikey='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtaWdnYXJjaWE5MEBnbWFpbC5jb20iLCJqdGkiOiIwZWE4ZjI2YS02MGNiLTQzMDktOTE0OC0wMTU1ZDY3YTExYjgiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTU4NTg3MDkwOSwidXNlcklkIjoiMGVhOGYyNmEtNjBjYi00MzA5LTkxNDgtMDE1NWQ2N2ExMWI4Iiwicm9sZSI6IiJ9.44EAKSmbmHyPLJIoLCLXeFbpV-CtOt-KZajfvvDNN44'
@@ -42,31 +71,17 @@ except ApiException as e:
     print("Exception when calling ObservacionConvencionalApi->datos_de_observacin__tiempo_actual_1: %s\n" % e)
     
 r_obsact = requests.get(api_response_obsact.to_dict()['datos'])
-df_data_obsact = pd.read_json(json.dumps(r_obsact.json()))
 meta_obsact = (requests.get(api_response_obsact.to_dict()['metadatos'])).json()
+pprint(meta_obsact)
+month_day = r_obsact.json()[0]['fint'][5:10].replace('-','')
+pprint(month_day)
+# df_data_obsact = pd.read_json(json.dumps(r_obsact.json()))
 
-with open('./data_obsact_{}.csv'.format(date_obsact),'w', encoding='utf-8') as f:
+
+with open('./data_obsact_{}.json'.format(month_day),'w', encoding='utf-8') as f:
     f.write(r_obsact.text)
     f.close()
 
-print(df_data_obsact)
-# pprint(meta)
 
-# data_radiation = r.text[31:]
-# pprint(data_radiation)
-# type(data_radiation)
-# pprint(data_radiation[0:20])
-# r.close()
-# type(data_radiation)
+plotting_data_obsact()
 
-df_data_obsact.fint = pd.to_datetime(df_data_obsact['fint'],format='%Y-%m-%d')
-# df['fecha'], format='%Y-%m-%d'
-
-day = df_data_obsact.fint[0].day #.split('-')[2][0:2]
-print(day)
-
-df_data_obsact.plot(x='fint', y=['ta', 'tpr', 'pres'], kind='line', grid=True,
-                    title='Day {}'.format(day), xlabel='Hour', secondary_y='pres')
-
-plt.show()
-print(df_data_obsact)
